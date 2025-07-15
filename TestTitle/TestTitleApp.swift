7aea2fbc47ca13006e7f995bfc6bed455565d5ca
Yuliia Marych <yuliia.marych@copecart.com>
2025-07-15 16:10:04 +0300

    made initial setup

---
+++ TestTitle/TestTitleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTitleApp.swift
+//  TestTitle
+//
+//  Created by Yuliia Marych on 15.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestTitleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
