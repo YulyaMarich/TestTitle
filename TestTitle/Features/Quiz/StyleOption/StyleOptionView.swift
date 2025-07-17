ab5b716e4bc1d7bc92ebc1816576e030fe1f74b0
Yuliia Marych <yuliia.marych@copecart.com>
2025-07-17 12:53:20 +0300

    updated StyleOptionView to handle async image

--- TestTitle/Features/Quiz/StyleOption/StyleOptionView.swift
+++ TestTitle/Features/Quiz/StyleOption/StyleOptionView.swift
@@ -9,36 +9,48 @@ import SwiftUI
 
 struct StyleOptionView: View {
     var isSelected: Bool
-    var image: UIImage
+    var imageURL: URL?
     var title: String
     var onTap: () -> Void
     
     var body: some View {
         SelectableCard(isSelected: isSelected) {
-            ZStack(alignment: .topTrailing) {
-                VStack(spacing: 4) {
-                    GeometryReader { geo in
-                        ZStack(alignment: .topTrailing) {
-                            Image(uiImage: image)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: geo.size.width, height: geo.size.height)
-                                .clipped()
-                            
-                            CheckBoxView(isChecked: isSelected) {
-                                onTap()
+            VStack(spacing: 4) {
+                GeometryReader { geo in
+                    ZStack(alignment: .topTrailing) {
+                        AsyncImage(url: imageURL) { phase in
+                            switch phase {
+                            case .empty:
+                                Color.clear
+                                    .overlay(ProgressView(), alignment: .center)
+                            case .success(let image):
+                                image
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: geo.size.width, height: geo.size.height)
+                                    .clipped()
+                                
+                            case .failure:
+                                Color.gray
+                                    .overlay(Image(systemName: "photo"))
+                            @unknown default:
+                                EmptyView()
                             }
-                            .frame(width: 20, height: 20)
                         }
+                        
+                        CheckBoxView(isChecked: isSelected) {
+                            onTap()
+                        }
+                        .frame(width: 20, height: 20)
                     }
-                    
-                    Text(title.uppercased())
-                        .font(.poppins(size: 13, weight: isSelected ? .medium : .light))
-                        .foregroundColor(.textPrimary)
-                        .multilineTextAlignment(.center)
                 }
-                .padding(8)
+                
+                Text(title.uppercased())
+                    .font(.poppins(size: 13, weight: isSelected ? .medium : .light))
+                    .foregroundColor(.textPrimary)
+                    .multilineTextAlignment(.center)
             }
+            .padding(8)
         } onTap: {
             onTap()
         }
